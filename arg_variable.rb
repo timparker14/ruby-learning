71eeafde50f28cdbe73d8767e213bb69fff25c58
timparker14 <tim.parker14@ntlworld.com>
2017-03-08 16:20:12 +0000

    Add files via upload

---
+++ arg_variable.rb
@@ -0,0 +1,5 @@
+first, second, third = ARGV
+
+$stdin.gets.chomp "Your first variable is: #{first}"
+$stdin.gets.chomp "Your second variable is: #{second}"
+$stdin.gets.chomp "Your third variable is: #{third}"
